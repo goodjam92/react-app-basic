d1f2800f0d197e9059a630032d98699181f12cc9
goodjam92 <devgoozz@gmail.com>
2022-10-23 17:39:21 +0900

    useEffect 사용방법
    -useEffect = 특정 조건일 때 함수를 실행해주는 기능
    -userEffect(function, deps);
    -deps([]) -> React가 변화를 체크하는 값
    -기본 포맷인 []를 사용하면 처음 1번만 실행 됨. (다른 상태 변경에 의해 다시 함수가 실행되지 않음)
    - [] 안에 상태 변화를 체크하고자 하는 값을 넣으면 그 값의 상태 변화에 따라 함수를 실행하거나 또는 재실행 하도록 할 수 있음.
    - [] 안에는 2개 이상의 값도 체크하도록 할 수 있음.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import Button from "./Button";
-import styled from "./App.module.css";
+import { useState, useEffect } from "react";
+import styled from "./Button.module.css";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  // userEffect(function, []) => 특정 조건일 때 실행하도록 해주는 기능
+  // 기본 포맷의 []를 사용하면 상태를 체크하는 값이 없기에 그저 1번만 실행 함.
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  // 어떤 특정한 값에 변화를 감지하여 실행하려면 [] 안에 value keyword를 넣음.
+  // useEffect의 실행 함수 안에서 (if)특정 조건을 부여할 수 있음
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  // 상태를 체크 할 값은 여러 값의 사용이 가능
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styled.title}>Welcome Back!!!</h1>
-      <Button text="Continue" />
-      <Button text="Continue" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here.."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick} className={`${styled.btn} ${styled.btnFont}`}>
+        click me
+      </button>
     </div>
   );
 }
